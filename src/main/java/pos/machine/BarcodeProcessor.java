785050ac2a0a597e8447a23e57c7fe2784824ec9
NSC/Network Service Center <NSC@oocl.com>
2020-11-26 09:58:38 +0800

    [POSMachine][Dominic Leung] feat: add class BarcodeProcessor

---
+++ src/main/java/pos/machine/BarcodeProcessor.java
@@ -0,0 +1,28 @@
+package pos.machine;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BarcodeProcessor {
+    public ArrayList<ProductInfo> processBarcodes(List<String> barcodes) {
+        ArrayList<ProductInfo> itemsInformation = new ArrayList<ProductInfo>();
+        List<String> unique_barcodes = barcodes.stream().distinct().collect(Collectors.toList());
+        unique_barcodes.forEach(barcode -> itemsInformation.add(retrieveItemDetail(barcode)));
+        return findQuantityOfEachItem(barcodes, itemsInformation);
+    }
+
+    private ProductInfo retrieveItemDetail(String barcode) {
+        List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
+        ItemInfo item = itemInfos.stream().filter(ItemInfo -> ItemInfo.getBarcode().equals(barcode)).findFirst().get();
+        return new ProductInfo(item.getBarcode(), item.getName(), item.getPrice(), 0, 0);
+    }
+
+    private ArrayList<ProductInfo> findQuantityOfEachItem(List<String> barcodes, ArrayList<ProductInfo> itemsInformation) {
+        itemsInformation.forEach(itemInfo -> {
+            itemInfo.setQuantity(Collections.frequency(barcodes, itemInfo.getBarcode()));
+        });
+        return itemsInformation;
+    }
+}
--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -7,29 +7,11 @@ import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        ArrayList<ProductInfo> itemsInformation = processBarcodes(barcodes);
+        BarcodeProcessor barcodeProcessor=new BarcodeProcessor();
+        ArrayList<ProductInfo> itemsInformation = barcodeProcessor.processBarcodes(barcodes);
         Receipt receipt=new Receipt(itemsInformation);
         return receipt.generateReceipt();
     }
 
-    private ArrayList<ProductInfo> processBarcodes(List<String> barcodes) {
-        ArrayList<ProductInfo> itemsInformation = new ArrayList<ProductInfo>();
-        List<String> unique_barcodes = barcodes.stream().distinct().collect(Collectors.toList());
-        unique_barcodes.forEach(barcode -> itemsInformation.add(retrieveItemDetail(barcode)));
-        return findQuantityOfEachItem(barcodes, itemsInformation);
-    }
-
-    private ProductInfo retrieveItemDetail(String barcode) {
-        List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
-        ItemInfo item = itemInfos.stream().filter(ItemInfo -> ItemInfo.getBarcode().equals(barcode)).findFirst().get();
-        return new ProductInfo(item.getBarcode(), item.getName(), item.getPrice(), 0, 0);
-    }
-
-    private ArrayList<ProductInfo> findQuantityOfEachItem(List<String> barcodes, ArrayList<ProductInfo> itemsInformation) {
-        itemsInformation.forEach(itemInfo -> {
-            itemInfo.setQuantity(Collections.frequency(barcodes, itemInfo.getBarcode()));
-        });
-        return itemsInformation;
-    }
 
 }
